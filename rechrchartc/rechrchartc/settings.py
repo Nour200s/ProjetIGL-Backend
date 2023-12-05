8b3fc6d51fa3a6edf0312062047603eca6ddd15a
imed3131 <129051822+imed3131@users.noreply.github.com>
2023-12-05 12:24:11 +0100

    connection avec db

--- rechrchartc/rechrchartc/settings.py
+++ rechrchartc/rechrchartc/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'rechrchartc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'WalrFDdofcpmeZ26',
+        'HOST': 'db.iiwzcaocnyayvtgyyfcg.supabase.co',
+        'PORT': '5432',
     }
 }
 
