5f66da17ec7e0006538c77acb17255d4a6a7e27c
Folokee <lr_menassel@esi.dz>
2023-12-12 12:38:42 +0100

    models created

--- rechrchartc/rchrchArtcl/models.py
+++ rechrchartc/rchrchArtcl/models.py
@@ -1,7 +1,48 @@
 from django.db import models
 
-class User(models.Model):
+class Admins(models.Model):
+     id = models.BigAutoField(primary_key=True)
      name = models.CharField(max_length=255)
      password = models.CharField(max_length=255)
+
+class Moderateurs(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     name = models.CharField(max_length=255)
+     password = models.CharField(max_length=255)
+
+class Article(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     titre = models.CharField(max_length=255)
+     resume = models.TextField()
+     contenu = models.TextField()
+     pdf = models.TextField()
+
+class User(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     name = models.CharField(max_length=255)
+     password = models.CharField(max_length=255)
+     favoris = models.ManyToManyField(Article)
+
+class Institution(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     nom = models.CharField(max_length=255)
+     adresse = models.CharField(max_length=255)
+
+
+class Auteurs(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     institutions = models.ForeignKey(Institution,on_delete=models.CASCADE)
+     nom = models.CharField(max_length=255)
+     email = models.CharField(max_length=255)
+     articles = models.ManyToManyField(Article)
+
+class References(models.Model):
+     id = models.BigAutoField(primary_key=True)
+     article = models.ForeignKey(Article ,on_delete=models.CASCADE)
+     contenu = models.CharField(max_length=512)
+
+
+     
+
 
 # Create your models here.
